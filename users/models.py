f8a60601cc745d43818e422a8f73fc78aff33ea5
Marc Jerome <mjt.eight0five@gmail.com>
2020-04-14 21:47:02 +0800

    Fixed get_full_name to include middle initial

--- users/models.py
+++ users/models.py
@@ -8,7 +8,11 @@ class CustomUser(AbstractUser):
     middle_name = models.CharField(max_length=30, blank=True, null=True)
     bio = models.TextField(max_length=300, blank=True)
 
-
     def __str__(self):
         return self.username
 
+    def get_full_name(self):
+        if self.middle_name is None:
+            return super(CustomUser, self).get_full_name()
+        else:
+            return "%s %s. %s" % (self.first_name, self.middle_name[0], self.last_name)

e856aac427158f22766ff781bf81970e692ddef3
Marc Jerome <mjt.eight0five@gmail.com>
2020-04-16 07:11:41 +0800

    Added eventListener to Add tag btn

--- preachings/forms.py
+++ preachings/forms.py
@@ -5,10 +5,10 @@ from ckeditor.widgets import CKEditorWidget
 
 
 class PreachingForm(ModelForm):
-
+    tags = forms.CharField(max_length=100, required=True)
     class Meta:
         model = Preaching
-        fields = ['title', 'text', 'date', 'privacy']   
+        fields = ['title', 'text', 'date', 'privacy', 'tags']   
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),

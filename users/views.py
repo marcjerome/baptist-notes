b319239dc7e3c6e1aaeb1f0bf8e62d65eab143a3
Marc Jerome <mjt.eight0five@gmail.com>
2020-04-29 13:29:56 +0800

    Added user profile for each users

--- users/views.py
+++ users/views.py
@@ -1,21 +1,24 @@
 from django.conf import settings
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth import get_user_model
-from django.views.generic import DetailView, CreateView
+from django.views.generic import DetailView, CreateView, ListView
 from django.views import View
 from django.contrib.auth import logout
 from preachings.models import Preaching
 from .forms import CustomUserCreationForm 
 
-class UserDetailView(DetailView):
+class UserDetailView(ListView):
     template_name = 'users/user_detail.html'
+    context_object_name = 'user'
 
+    #Why can't I logout?!
     def get_queryset(self):
-        return get_user_model().objects.filter(pk=self.kwargs['pk'])
+        self.user = get_user_model().objects.get(pk=self.kwargs['pk'])
+        return self.user
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['preachings'] = Preaching.objects.filter(user=get_user_model().objects.get(pk=self.kwargs['pk']))
+        context['preachings'] = Preaching.objects.filter(user=self.user, privacy=False)
         return context
 
 class RegisterView(CreateView):
@@ -24,6 +27,5 @@ class RegisterView(CreateView):
 
 
     def get_success_url(self):
-        
         return self.request.GET.get('next')
         
